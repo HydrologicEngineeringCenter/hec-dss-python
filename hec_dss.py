46abef381a188cde5dd1188848290e16e9a7f009
Karl Tarbet <karl.l.tarbet@usace.army.mil>
2023-08-22 07:50:00 -0700

    create native layer hec_dss_native.py

---
+++ hec_dss.py
@@ -0,0 +1,57 @@
+from hec_dss_native import HecDssNative
+from datetime import datetime
+class HecDss:
+
+    def __init__(self,filename):
+        self._native = HecDssNative()
+        self._native.hec_dss_open(filename)
+	
+    def get(self,pathname,startDateTime, endDateTime):
+        # get sizes
+        startDate = startDateTime.strftime('%d%b%Y')
+        startTime = startDateTime.strftime('%H:%M')
+        endDate = endDateTime.strftime('%d%b%Y')
+        endTime = startDateTime.strftime('%H:%M')
+        numberValues = [0] # using array to allow modification
+        qualityElementSize = [0]
+        self._native.hec_dss_tsGetSizes(pathname,
+                            startDate, startTime,
+                            endDate, endTime,numberValues,qualityElementSize)
+        print("Number of values:", numberValues[0])
+        print("Quality element size:", qualityElementSize[0])
+
+        
+        # tsRetrive
+
+        times=[]
+        values=[]
+        numberValuesRead=[0]
+        quality=[]
+        julianBaseDate =[0]
+        timeGranularitySeconds =[0]
+        units = [""]
+        bufferLength=40
+        dataType =[""]
+
+
+        self._native.hec_dss_tsRetrieve(pathname, 
+        startDate, startTime, endDate, endTime, times,
+        values, numberValues[0],
+        numberValuesRead, quality, qualityElementSize[0], 
+        julianBaseDate, timeGranularitySeconds,
+        units, bufferLength, dataType, bufferLength)
+
+        print("units = "+units[0])
+        print("datatype = "+dataType[0])
+
+
+    def recordCount(self):
+        return self._native.hec_dss_record_count()
+			
+#import pdb;pdb.set_trace()
+print("hi")
+dss = HecDss("sample7.dss")
+print("record count = "+str(dss.recordCount()))
+t1 = datetime(1924, 1, 1)
+t2 = datetime(2005, 1 ,1)
+tsc = dss.get("//SACRAMENTO/PRECIP-INC//1Day/OBS/",t1,t2)
---
+++ hec_dss_native.py
@@ -0,0 +1,212 @@
+import ctypes
+from ctypes import c_char, c_double, c_int,byref, create_string_buffer
+
+
+class HecDssNative:
+    def __init__(self):
+        x = r"C:\project\hec-dss\heclib\hecdss\x64\Release\hecdss.dll"
+        #self.dll = ctypes.CDLL("hecdss")
+        self.dll = ctypes.CDLL(x)
+    
+    def hec_dss_open(self,dss_filename):
+        self.dll.hec_dss_open.argtypes = [ctypes.c_char_p, ctypes.POINTER(ctypes.c_void_p)]
+        self.dll.hec_dss_open.restype = ctypes.c_int
+        self.handle = ctypes.c_void_p()
+        rval = self.dll.hec_dss_open(dss_filename.encode('utf-8'),ctypes.byref(self.handle))
+        if rval == 0:
+            print("DSS file opened successfully.")
+        else:
+            print("Error opening DSS file.")
+        return rval
+
+    def hec_dss_close(self):
+        f = self.dll.hec_dss_close
+        f.argtypes = [ctypes.c_void_p()]
+        f.restype = ctypes.c_int
+        return f(self.handle)
+		
+    def hec_dss_record_count(self):
+        f = self.dll.hec_dss_record_count
+        f.argtypes = [ctypes.c_void_p()]
+        f.restype = ctypes.c_int
+        return f(self.handle)
+
+    def hec_dss_export_to_file(self,path,outputFile,startDate,startTime,endDate,endTime):
+        
+        self.dll.hec_dss_export_to_file.argtypes = [
+            ctypes.c_void_p(),  # dss
+            ctypes.c_char_p,  # path
+            ctypes.c_char_p,  # outputFile
+            ctypes.c_char_p,  # startDate
+            ctypes.c_char_p,  # startTime
+            ctypes.c_char_p,  # endDate
+            ctypes.c_char_p   # endTime
+        ]
+        self.dll.hec_dss_export_to_file.restype = ctypes.c_int
+
+        result = self.dll.hec_dss_export_to_file(self.handle,path, outputFile, startDate, startTime, endDate, endTime)
+
+    def hec_dss_tsGetSizes(self,pathname,
+                            startDate, startTime,
+                            endDate, endTime,numberValues,qualityElementSize):
+        self.dll.hec_dss_tsGetSizes.argtypes = [
+            ctypes.c_void_p(), # dss
+            ctypes.c_char_p,  # path
+            ctypes.c_char_p,  # startDate
+            ctypes.c_char_p,  # startTime
+            ctypes.c_char_p,  # endDate
+            ctypes.c_char_p,  # endTime
+            ctypes.POINTER(ctypes.c_int), #numberValues
+            ctypes.POINTER(ctypes.c_int)  #qualityElementSize
+        ]
+        self.dll.hec_dss_tsGetSizes.restype = ctypes.c_int
+        
+        nv = ctypes.c_int()
+        qes = ctypes.c_int()
+
+        result = self.dll.hec_dss_tsGetSizes(self.handle, pathname.encode('utf-8'),
+                                            startDate.encode('utf-8'), startTime.encode('utf-8'),
+                                            endDate.encode('utf-8'), endTime.encode('utf-8'),
+                                            ctypes.byref(nv),
+                                            ctypes.byref(qes))
+
+        numberValues[0] = nv.value
+        qualityElementSize[0] = qes.value
+        #import pdb;pdb.set_trace()
+
+        if result == 0:
+            print("Function call successful:")
+            print("Number of values:", numberValues[0])
+            print("Quality element size:", qualityElementSize[0])
+        else:
+            print("Function call failed with result:", result)
+
+        return result
+        
+    def hec_dss_tsRetrieve(self,pathname, 
+        startDate, startTime, endDate, endTime, 
+        times, values, arraySize,
+        numberValuesRead, quality, qualityLength, 
+        julianBaseDate, timeGranularitySeconds,
+        units, unitsLength, dataType, typeLength):
+
+        f = self.dll.hec_dss_tsRetrieve
+        f.argtypes = [
+            ctypes.c_void_p,           # dss
+            ctypes.c_char_p,           # pathname
+            ctypes.c_char_p,           # startDate
+            ctypes.c_char_p,           # startTime
+            ctypes.c_char_p,           # endDate
+            ctypes.c_char_p,           # endTime
+            ctypes.POINTER(ctypes.c_int),    # timeArray
+            ctypes.POINTER(ctypes.c_double), # valueArray
+            ctypes.c_int,              # arraySize
+            ctypes.POINTER(ctypes.c_int),    # numberValuesRead
+            ctypes.POINTER(ctypes.c_int),    # quality
+            ctypes.c_int,               # qualityLength
+            ctypes.POINTER(ctypes.c_int),    # julianBaseDate
+            ctypes.POINTER(ctypes.c_int),    # timeGranularitySeconds
+            ctypes.c_char_p,           # units
+            ctypes.c_int,              # unitsLength
+            ctypes.c_char_p,           # dataType
+            ctypes.c_int               # typeLength
+            ]
+        f.restype = ctypes.c_int
+
+        c_arraySize = c_int(arraySize)
+        c_times = (ctypes.c_int32 * arraySize)()
+        c_values = (c_double * arraySize)()
+        c_numberValuesRead = c_int(0)
+        size = qualityLength*arraySize
+
+        c_quality =(c_int * size)()
+        c_julianBaseDate = c_int(0)
+        c_timeGranularitySeconds = c_int(0)
+  
+        buff_size = unitsLength
+        c_units = create_string_buffer(buff_size)
+        c_unitsLength = c_int(buff_size)
+  
+        buff_size = typeLength
+        c_dataType = create_string_buffer(buff_size)
+        c_typeLength =  c_int(buff_size)
+        c_julianBaseDate = c_int()
+        c_timeGranularitySeconds = c_int()
+
+        rval = f(self.handle,
+          pathname.encode('utf-8'), 
+          startDate.encode('utf-8'),
+          startTime.encode('utf-8'), 
+          endDate.encode('utf-8'), 
+          endTime.encode('utf-8'), 
+          c_times, 
+          c_values, 
+          c_arraySize,
+          byref(c_numberValuesRead), 
+          c_quality, 
+          qualityLength, 
+          byref(c_julianBaseDate), 
+          byref(c_timeGranularitySeconds),
+          c_units, 
+          c_unitsLength, 
+          c_dataType, 
+          c_typeLength)
+
+        units[0] = c_units.value.decode('utf-8')
+        dataType[0] = c_dataType.value.decode('utf-8')
+        #times         
+        return rval
+
+		
+    def test():
+        dss = HecDssNative()
+        outputFile=b"output.txt"
+
+        dss.hec_dss_open(b"sample7.dss")
+        nnn = dss.hec_dss_record_count()
+        print ("record count = "+str(nnn))		
+
+
+        times = [0,0,0,0,0]
+        numberValues =len(times)
+        times_int = (ctypes.c_int * numberValues)(*times)
+        values = [0,0,0,0,0]
+        values_double = (ctypes.c_double * numberValues)(*values)
+
+        numberValuesRead = ctypes.c_int()
+        quality = []
+        qualityLength = ctypes.c_int(len(quality))
+        quality_int = (ctypes.c_int * len(quality))(*quality)
+
+        julianBaseDate = ctypes.c_int()
+        timeGranularitySeconds = ctypes.c_int()
+        buffer_size = 20
+        units = ctypes.create_string_buffer(buffer_size)
+        dataType = ctypes.create_string_buffer(buffer_size)
+
+        result = dss.hec_dss_tsRetrieve(b"//SACRAMENTO/PRECIP-INC//1Day/OBS/", 
+                b"01Jan1924",b"0100", b"01Jan2005", b"2400", times_int, values_double, numberValues,
+                ctypes.byref(numberValuesRead), quality_int, qualityLength, ctypes.byref(julianBaseDate), ctypes.byref(timeGranularitySeconds),
+                units, buffer_size, dataType, buffer_size)
+
+        if result == 0:
+            print("Function call successful.")
+        else:
+            print("Function call failed."+str(result))
+
+        print(units.value.decode('utf-8'))
+
+
+        # ppp = b"/AMERICAN/FOLSOM/FLOW-RES IN/01JAN2006/1Day/OBS/"
+        # sd = b"12MAR2006"
+        # ed = b"05APR2006"
+        # st = b"1200"
+        # et = b"1200"
+        # dss = HecDss()
+        # outputFile=b"output.txt"
+        # 
+        # dss.open("sample7.dss")
+        # nnn = dss.record_count()
+        # print ("record count = "+str(nnn))
+        # ttt = dss.hec_dss_export_to_file(ppp,outputFile,sd,st,ed,et)
+        # dss.close()

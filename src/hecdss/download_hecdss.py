022d31268a1cc65a85a1371fc582b508e48047c2
Florian Kluibenschadl <fkluibenschaedl@rti.org>
2023-09-25 16:30:14 -0600

    Updates download_hecdss script and makes it available after install

--- src/hecdss/download_hecdss.py
+++ src/hecdss/download_hecdss.py
@@ -3,45 +3,48 @@
 from pathlib import Path
 import shutil
 import sys
-import requests
+import tempfile
 import zipfile
 import os
 
+import urllib.request as request
 
-def download_and_unzip(url, zip_file, destination_dir):
+
+def download_and_unzip(url, destination_dir):
     """Retrieves a compressed archive from the URL and extracts it in the destination dir.
 
     Parameters
     ----------
     url : str
         URL to the archive.
-    zip_file : str
-        Name of the local target archive.
     destination_dir :
         Path to the local directory where the archive content is extracted to.
+
     """
     print(url)
     os.makedirs(destination_dir, exist_ok=True)
-    response = requests.get(zip_url, timeout=300)
-    if response.status_code == 200:
-        zip_file_path = os.path.join(destination_dir, zip_file)
-        with open(zip_file_path, "wb") as zip_file:
-            zip_file.write(response.content)
-        print("Zip file downloaded successfully.")
+    with request.urlopen(url) as response:
+        if response.status == 200:
+            with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+                shutil.copyfileobj(response, tmp_file)
+                with zipfile.ZipFile(tmp_file) as zip_ref:
+                    zip_ref.extractall(destination_dir)
+            print(f"Contents extracted successfully in {destination_dir!r}")
 
-        with zipfile.ZipFile(zip_file_path, "r") as zip_ref:
-            zip_ref.extractall(destination_dir)
-        print("Contents extracted successfully.")
-        os.remove(zip_file_path)
-    else:
-        print(f"Failed to download zip file. Status code: {response.status_code}")
+        else:
+            print(f"Failed to download zip file. Status code: {response.status_code}")
 
 
 def run():
+    """Downloads and extracts the binary shared library into the current python environment."""
     destination_dir = Path(__file__).parent.joinpath("lib")
     if sys.platform == "linux" or sys.platform == "darwin":
         zip_url = "https://www.hec.usace.army.mil/nexus/repository/maven-public/mil/army/usace/hec/hecdss/7-IS-win-x86_64/hecdss-7-IS-win-x86_64.zip"
-        download_and_unzip(zip_url, "hecdss-7-IS-win-x86_64.zip", destination_dir)
+        download_and_unzip(zip_url, destination_dir)
     elif sys.platform == "win32":
         zip_url = "https://www.hec.usace.army.mil/nexus/repository/maven-public/mil/army/usace/hec/hecdss/7-IS-linux-x86_64/hecdss-7-IS-linux-x86_64.zip"
-        download_and_unzip(zip_url, "hecdss-7-IS-linux-x86_64.zip", destination_dir)
+        download_and_unzip(zip_url, destination_dir)
+
+
+if __name__ == "__main__":
+    run()

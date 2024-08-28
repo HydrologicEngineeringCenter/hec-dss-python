e1b3d90894dcca97ac8d6cb1645cad2c2a723981
Karl Tarbet <karl.l.tarbet@usace.army.mil>
2024-08-28 15:04:26 -0700

    remove some LD_LIBRARY_PATH magic, and PATH hacking

--- src/hecdss/__init__.py
+++ src/hecdss/__init__.py
@@ -1,18 +1,3 @@
-import os
-import sys
-import ctypes
-from pathlib import Path
-
-
-shared_lib_dir = Path(__file__).parent.joinpath("lib")
-
-if sys.platform == "linux" or sys.platform == "darwin":
-    try:
-        os.environ["LD_LIBRARY_PATH"] = str(shared_lib_dir) + os.pathsep + os.environ['LD_LIBRARY_PATH']
-    except KeyError:
-        os.environ["LD_LIBRARY_PATH"] = str(shared_lib_dir)
-else:
-    os.environ['PATH'] = f"{shared_lib_dir};{os.environ['PATH']}"
 
 from hecdss.catalog import Catalog
 from hecdss.hecdss import HecDss

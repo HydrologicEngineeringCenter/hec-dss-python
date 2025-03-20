a2224705f8c7f191086efb5d585115f9a8d0a6c0
Karl Tarbet <karl.l.tarbet@usace.army.mil>
2025-03-20 15:03:09 -0700

    documentation

--- Readme.md
+++ Readme.md
@@ -110,11 +110,11 @@ This library uses (`hecdss.dll` on Windows and `libhecdss.so` on Unix/Linux).  h
    1. One option is to download it from provided link [hecdss.dll](https://github.com/HydrologicEngineeringCenter/hec-dss-python/releases/download/v0.0.1-alpha/hecdss.dll)
    2. Second option is to run the following python script in the repository directory:
       ```
-      C:\project\hec-dss-python>python src\hec-dss-python\download_hecdss.py
+      python src\hec-dss-python\download_hecdss.py
       ```
 3. from the repo directory (hec-dss-python)
    ```
-   C:\project\hec-dss-python>python tests\test_basics.py
+   python tests\test_basics.py
    ```
 
 
--- src/hecdss/paired_data.py
+++ src/hecdss/paired_data.py
@@ -4,6 +4,9 @@ import numpy as np
 
 class PairedData:
     def __init__(self):
+        """
+        Initialize a PairedData object with default values.
+        """
         self.id = None
         self.ordinates = np.empty(0)
         self.values = np.empty(0)
@@ -15,9 +18,24 @@ class PairedData:
         self.location_information = None
 
     def curve_count(self):
+        """
+        Retrieve the number of curves in the paired data.
+
+        Returns:
+        int: The number of curves.
+        """
         return len(self.values)
 
     # def to_data_frame(self, include_index=False):
+    #     """
+    #     Convert the paired data to a pandas DataFrame.
+    #
+    #     Parameters:
+    #     include_index (bool, optional): Whether to include an index column. Defaults to False.
+    #
+    #     Returns:
+    #     pandas.DataFrame: The paired data as a DataFrame.
+    #     """
     #     data = {"stage": self.ordinates}
     #     for i in range(len(self.values)):
     #         label = self.labels[i] if i < len(self.labels) else f"value{i + 1}"
@@ -30,6 +48,22 @@ class PairedData:
 
     @staticmethod
     def create(x_values, y_values, labels=[], x_units="", x_type="", y_units="", y_type="", path=None):
+        """
+        Create a PairedData object with the provided data.
+
+        Parameters:
+        x_values (list of float): The independent variable values.
+        y_values (list of list of float): The dependent variable values.
+        labels (list of str, optional): The labels for the dependent variables. Defaults to an empty list.
+        x_units (str, optional): The units of the independent variable. Defaults to an empty string.
+        x_type (str, optional): The type of the independent variable. Defaults to an empty string.
+        y_units (str, optional): The units of the dependent variable. Defaults to an empty string.
+        y_type (str, optional): The type of the dependent variable. Defaults to an empty string.
+        path (str, optional): The path identifier for the paired data. Defaults to None.
+
+        Returns:
+        PairedData: An instance of the PairedData class.
+        """
         pd = PairedData()
         pd.id = path
         pd.ordinates = np.array(x_values)
@@ -40,4 +74,3 @@ class PairedData:
         pd.type_independent = x_type
         pd.type_dependent = y_type
         return pd
-

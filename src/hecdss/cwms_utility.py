8972bb5769b91e3aa44efe7072b7382c5deb7a84
Karl Tarbet <karl.l.tarbet@usace.army.mil>
2025-03-21 13:58:42 -0700

    add notes and methods to cwms_utility.py

--- src/hecdss/cwms_utility.py
+++ src/hecdss/cwms_utility.py
@@ -1,39 +1,46 @@
 import re
 
-from hecdss import DssPath
+from hecdss import DssPath, RegularTimeSeries
 from hecdss.dss_type import DssType
 
 
 class CwmsUtility:
+    """
+    These Utility methods are used to convert between CWMS and DSS conventions
+    This is a short term solution.  Longer term we will be using the following library:
+    https://github.com/HydrologicEngineeringCenter/hec-python-library
+
+    """
+
     CWMS_DSS_INTERVAL_MAP = {
-        '1Minute': '1Minute',
-        '2Minutes': '2Minute',
-        '3Minutes': '3Minute',
-        '4Minutes': '4Minute',
-        '5Minutes': '5Minute',
-        '6Minutes': '6Minute',
-        '12Minutes': '12Minute',
-        '15Minutes': '15Minute',
-        '20Minutes': '20Minute',
-        '30Minutes': '30Minute',
-        '1Hour': '1Hour',
-        '2Hours': '2Hour',
-        '3Hours': '3Hour',
-        '4Hours': '4Hour',
-        '6Hours': '6Hour',
-        '8Hours': '8Hour',
-        '12Hours': '12Hour',
-        '1Day': '1Day',
+        "1Minute": "1Minute",
+        "2Minutes": "2Minute",
+        "3Minutes": "3Minute",
+        "4Minutes": "4Minute",
+        "5Minutes": "5Minute",
+        "6Minutes": "6Minute",
+        "12Minutes": "12Minute",
+        "15Minutes": "15Minute",
+        "20Minutes": "20Minute",
+        "30Minutes": "30Minute",
+        "1Hour": "1Hour",
+        "2Hours": "2Hour",
+        "3Hours": "3Hour",
+        "4Hours": "4Hour",
+        "6Hours": "6Hour",
+        "8Hours": "8Hour",
+        "12Hours": "12Hour",
+        "1Day": "1Day",
         # # DSS doesn't support the 2Day, 3Day, etc...  intervals.
         # '2Days': '',
         # '3Days': '',
         # '4Days': '',
         # '5Days': '',
         # '6Days': '',
-        '1Week': '1Week',
-        '1Month': '1Month',
-        '1Year': '1Year',
-        '0': 'Ir-Month'  # not optimized for some cases
+        "1Week": "1Week",
+        "1Month": "1Month",
+        "1Year": "1Year",
+        "0": "Ir-Month",  # not optimized for some cases
     }
 
     # build another map from DSS to CWMS (use lower case for DSS key)
@@ -43,8 +50,6 @@ class CwmsUtility:
     DSS_CWMS_INTERVAL_MAP["ir-year"] = "0"
     DSS_CWMS_INTERVAL_MAP["ir-decade"] = "0"
     DSS_CWMS_INTERVAL_MAP["ir-century"] = "0"
-
-
 
     @staticmethod
     def get_cwms_parameter_type(dss_type: DssType):
@@ -85,7 +90,7 @@ class CwmsUtility:
         # [Interval]
         tsid += CwmsUtility._convert_dss_interval_to_cwms_interval(e) + "."
         # [Duration]
-        tsid += duration+"."
+        tsid += duration + "."
         # [Version]
         tsid += f
         if strict:
@@ -103,10 +108,12 @@ class CwmsUtility:
 
         """
         # Be sure there are no slashes used anywhere in the name
-        ts_id = ts_id.replace('/', '-')
+        ts_id = ts_id.replace("/", "-")
         # Replace period separators with slashes so we can split it
-        ts_id = ts_id.replace('.', '/')
-        location, parameter, cwms_type, cwms_interval, duration, version = ts_id.split('/')[:6]
+        ts_id = ts_id.replace(".", "/")
+        location, parameter, cwms_type, cwms_interval, duration, version = ts_id.split(
+            "/"
+        )[:6]
 
         path = DssPath("///////", 0)
         path.A = ""
@@ -128,11 +135,13 @@ class CwmsUtility:
         if cwms_interval in CwmsUtility.CWMS_DSS_INTERVAL_MAP:
             return CwmsUtility.CWMS_DSS_INTERVAL_MAP[cwms_interval]
 
-        if len(cwms_interval) > 1 and cwms_interval[0] == '~':  # pseudo regular
+        if len(cwms_interval) > 1 and cwms_interval[0] == "~":  # pseudo regular
             if cwms_interval[1:] in CwmsUtility.CWMS_DSS_INTERVAL_MAP:
                 return "~" + CwmsUtility.CWMS_DSS_INTERVAL_MAP[cwms_interval[1:]]
 
-        raise ValueError(f"The cwms interval '{cwms_interval}' is invalid, or there is not a supported conversion to DSS")
+        raise ValueError(
+            f"The cwms interval '{cwms_interval}' is invalid, or there is not a supported conversion to DSS"
+        )
 
     @staticmethod
     def _convert_dss_interval_to_cwms_interval(dss_interval: str):
@@ -144,13 +153,14 @@ class CwmsUtility:
         if e_part in CwmsUtility.DSS_CWMS_INTERVAL_MAP:
             return CwmsUtility.DSS_CWMS_INTERVAL_MAP[e_part]
 
-        if len(e_part) > 1 and e_part[0] == '~':
+        if len(e_part) > 1 and e_part[0] == "~":
             if e_part[1:] in CwmsUtility.DSS_CWMS_INTERVAL_MAP:
                 return "~" + CwmsUtility.DSS_CWMS_INTERVAL_MAP[e_part[1:]]
 
-        raise ValueError(f"The dss interval '{dss_interval}' is invalid, or there is not a supported conversion to a "
-                         f"CWMS interval")
-
+        raise ValueError(
+            f"The dss interval '{dss_interval}' is invalid, or there is not a supported conversion to a "
+            f"CWMS interval"
+        )
 
     @staticmethod
     def dss_data_type_from_cwms_tsid(cwms_tsid) -> str:
@@ -160,7 +170,7 @@ class CwmsUtility:
         :param cwms_tsid: input time-series identifier
         :return: string representing a dss data type
         """
-        parts = cwms_tsid.split('.')
+        parts = cwms_tsid.split(".")
         parameter = parts[1]
         ts_type = parts[2]
         if ts_type == "Inst":
@@ -179,3 +189,52 @@ class CwmsUtility:
         else:
             dss_type = ts_type  # default: use cwms type
         return str(dss_type)
+
+    @staticmethod
+    def cwms_to_regular_timeseries(data):
+        """
+        Convert a CWMS (pandas-dataframe) TimeSeries to a RegularTimeSeries
+        """
+        times = data.df["date-time"].to_list()
+        values = data.df["value"].to_list()
+        cwms_tsid = data.json["name"]
+        data_type = CwmsUtility.dss_data_type_from_cwms_tsid(cwms_tsid)
+        units = data.json["units"]
+        dss_path = CwmsUtility.cwms_ts_id_to_dss_path(cwms_tsid)
+
+        if dss_path.E[:2].lower() == "ir":
+            raise ValueError(
+                f"Irregular time series not implemented in cwms_to_timeseries method input: {cwms_tsid}"
+            )
+
+        ts = RegularTimeSeries.create(
+            values,
+            times,
+            units=units,
+            dataType=str(data_type),
+            interval=dss_path.E,
+            path=str(dss_path),
+        )
+        ts.id = str(dss_path)
+        return ts
+
+    @staticmethod
+    def regular_time_series_to_json(rts: RegularTimeSeries, office_id: str, ts_id: str):
+        """
+        converts a regular timeseries into a JSON format that is
+        compatible with the CWMS data API
+        """
+        # hard code flag = 0
+        flag = 0
+        # dss quality code is the same cwms
+        values = [[d, v, flag] for d, v in zip(rts.times, rts.values)]
+
+        json_result = {
+            "name": ts_id,
+            "office-id": office_id,
+            "units": rts.units,
+            "values": values,
+            "version-date": None,
+        }
+
+        return json_result
